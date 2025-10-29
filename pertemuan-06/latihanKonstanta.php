460fda1335e084123ba8cf98b7647e11dd584d75
Widya Serena Mulyaputeri <2511500020@mahasiswa.atmaluhur.ac.id>
2025-10-29 15:17:35 +0700

    latihan konstanta

--- pertemuan-06/latihanKonstanta.php
+++ pertemuan-06/latihanKonstanta.php
@@ -1,7 +1,11 @@
 <?php
 define("KAMPUS", "ISB Atma Luhur");
+define("DOSEN_PENGAMPU", "Bapak Yohanes Setiawan");
 const ANGKATAN = 2025;
+const MATAKULIAH = "Pemrograman Web Dasar";
 
 echo "Kampus: " . KAMPUS . "<br>";
-echo "Angkatan: " . ANGKATAN;
+echo "Angkatan: " . ANGKATAN . "<br>";
+echo "Dosen Pengampu: " . DOSEN_PENGAMPU . "<br>";
+echo "Mata Kuliah: " . MATAKULIAH . "<br>";
 ?>
