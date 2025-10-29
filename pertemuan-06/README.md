178b0bee3c6617fa86fc2390cca3bd9846a5b0e5
Widya Serena Mulyaputeri <2511500020@mahasiswa.atmaluhur.ac.id>
2025-10-29 16:52:16 +0700

    latihan operator dasar

--- pertemuan-06/README.md
+++ pertemuan-06/README.md
@@ -12,4 +12,5 @@ Hari ini, Rabu 22 Oktober 2025, saya belajar:
     <li>latihan tipe data</li>
     <li>latihan konstanta</li>
     <li>latihan komentar</li>
+    <li>latihan operator dasar</li>
 </ol>
---
+++ pertemuan-06/latihanOperatorDasar.php
@@ -0,0 +1,7 @@
+<?php
+$a = 10; //deklarasi variabel a bernilai 10
+$b = 3; //deklarasi variabel b bernilai 3
+
+echo $a + $b . "<br>"; //10 + 3 = 13
+echo $a % $b; //10 % 3 artinya 10/3 punya sisa 1
+?>
